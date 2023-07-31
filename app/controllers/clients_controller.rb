6965e0361ca9bc4426342e34f6e0a60c7b3db1c8
marco <enidev911@gmail.com>
2023-07-31 02:02:26 -0400

    up

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -25,7 +25,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to client_url(@client), notice: "Client was successfully created." }
+        format.html { redirect_to client_url(@client), notice: t('.notice') }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new, status: :unprocessable_entity }
